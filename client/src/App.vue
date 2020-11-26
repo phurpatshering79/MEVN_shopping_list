cea81d24863490838555e4725bda6007de581a27
Phurpa Sherpa <phurpa.tshering79@gmail.com>
2020-11-26 13:46:37 -0500

    Add header and footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <v-app>
-    <v-main class="smoothed">
-      <NavBar />
-    </v-main>
+    <v-app-bar app color="teal darken-1" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>To Do App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="toggleTheme" text rounded>
+        Toggle Theme
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer color="teal lighten-2" padless>
+      <v-layout justify-center wrap>
+        <v-flex teal lighten-2 py-4 text-center white--text xs12>
+          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import NavBar from './components/NavBar'
-
 export default {
   name: 'App',
-  components: {
-    NavBar
+  data() {
+    return {}
   },
-
-  data: () => ({
-    //
-  })
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.themes.dark.anchor = '#41B883'
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
+  }
 }
 </script>
